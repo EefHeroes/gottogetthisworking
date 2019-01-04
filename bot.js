ecac3b715949548b6537e6aa16d9ca34a17aad8e
Eef <40184375+EefHeroes@users.noreply.github.com>
2019-01-04 17:09:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,26 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+var Discord = require('discord.io');
+var logger = require('winston');
 
-client.on('ready', () => {
-    console.log('I am ready!');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: process.env.BOT_TOKEN,
+   autorun: true
 });
 
-client.on('message', function (user, userID, channelID, message, evt) {
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
@@ -20,12 +35,12 @@ client.on('message', function (user, userID, channelID, message, evt) {
             var coinflip = Math.random();
             
             if(coinflip <= 0.5){
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'Heads'
                 });
             }
-            else{client.sendMessage({
+            else{bot.sendMessage({
                     to: channelID,
                     message: 'Tails'
                 });
@@ -35,12 +50,12 @@ client.on('message', function (user, userID, channelID, message, evt) {
             var coinflip = Math.random();
             
             if(coinflip <= 0.5){
-                client.sendMessage({
+                bot.sendMessage({
                     to: channelID,
                     message: 'Heads'
                 });
             }
-            else{client.sendMessage({
+            else{bot.sendMessage({
                     to: channelID,
                     message: 'Tails'
                 });
@@ -50,5 +65,5 @@ client.on('message', function (user, userID, channelID, message, evt) {
      }
 
 });
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+
+//client.login();
